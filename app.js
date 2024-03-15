84ea08ef6b3addb6dd46b3b7f78b44002feff589
beki-jone <bekojone@gmail.com>
2024-03-15 22:57:42 +0300

    port

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 const app = express();
 // Enable CORS for all routes
 app.use(cors());
-const port = 5510;
+const port = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 const dbConnection = require("./db/dbConfig");
 const userRoutes = require("./routes/userRoute");
 const questionRoutes = require("./routes/questionRoute");
@@ -16,9 +19,9 @@ app.use("/api/answers", answerRoutes);
 async function start() {
   try {
     const result = await dbConnection.execute("select 'test' ");
-     app.listen(port);
-    console.log("Database connection established");
-    console.log(`Listening on ${port}`);
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
   } catch (error) {
     console.log(error.message);
   }
